b8975db24b2b09ada1a90a9f2b75da668ef77af8
David Hughes <hughesd9@tcd.ie>
2020-09-24 16:22:04 +0100

    Created a function, find_empty, to find empty squares on the Sudoku board

--- SudokuSolverMain.py
+++ SudokuSolverMain.py
@@ -24,4 +24,10 @@ def print_board(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
-print_board(board)
+#print_board(board)
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
