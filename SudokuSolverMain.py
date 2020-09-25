bb4a81d32fe928430e2893f5f47bd2b9c468efd7
David Hughes <hughesd9@tcd.ie>
2020-09-25 17:27:22 +0100

    Created function, is_valid, to check if the board is correct so far, and created function, solve, to solve the board using backtracking. Added some print functions to print the solved board.

--- SudokuSolverMain.py
+++ SudokuSolverMain.py
@@ -10,6 +10,46 @@ board = [
     [0,4,9,2,0,6,0,0,7]
     ]
 
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+def is_valid(board, number, pos):
+    #check row
+    for i in range(len(board)):
+        if board[pos[0]][i] == number and pos[1] != i:
+            return False
+
+    #check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == number and pos[0] != i:
+            return False
+
+    #check cell
+    cell_x = pos[1] // 3
+    cell_y = pos[0] // 3
+
+    for i in range(cell_y * 3, cell_y * 3 +3):
+        for j in range(cell_x * 3, cell_x * 3 +3):
+            if board[i][j] == number and (i, j) != pos:
+                return False
+
+    return True
+
 def print_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
@@ -31,3 +71,10 @@ def find_empty(board):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j)
+
+    return None
+
+print_board(board)
+solve(board)
+print("--------------------------")
+print_board(board)
